932cb677424573e9c6c2ebc96ee13837afb101e8
kh-6 <khawla6xavi@gmail.com>
2017-08-11 00:54:28 +0300

    just shim

--- ty4/Program.cs
+++ ty4/Program.cs
@@ -10,7 +10,7 @@ namespace ty4
     {
         static void Main(string[] args)
         {
-
+            //just shim
             Console.WriteLine("shim");
             Console.ReadKey();
         }
